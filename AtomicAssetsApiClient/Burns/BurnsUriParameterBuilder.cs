d94384c3c7203f3c2bc5ce22e7ba9d0cb70063e6
Ash Lee <ashleewawa17@gmail.com>
2022-06-13 12:39:30 +0000

    Update BurnsUriParameterBuilder

--- AtomicAssetsApiClient/Burns/BurnsUriParameterBuilder.cs
+++ AtomicAssetsApiClient/Burns/BurnsUriParameterBuilder.cs
@@ -27,72 +27,170 @@ namespace AtomicAssetsApiClient.Burns
 /* A nullable enum specifying the sortStrategy. */
         private SortStrategy? _sortStrategy;
 
+/// <summary>
+/// `WithCollectionName` is a function that takes a string as a parameter and returns an
+/// `BurnsUriParameterBuilder` object
+/// </summary>
+/// <param name="collectionName">The name of the collection you want to query.</param>
+/// <returns>
+/// The BurnsUriParameterBuilder object.
+/// </returns>
+
         public BurnsUriParameterBuilder WithCollectionName(string collectionName)
         {
             _collectionName = collectionName;
             return this;
         }
 
+
+/// <summary>
+/// > This function sets the schema name for the query
+/// </summary>
+/// <param name="schemaName">The name of the schema to use.</param>
+/// <returns>
+/// The AccountsUriParameterBuilder object.
+/// </returns>
         public BurnsUriParameterBuilder WithSchemaName(string schemaName)
         {
             _schemaName = schemaName;
             return this;
         }
 
+
+/// <summary>
+/// It sets the value of the templateId variable.
+/// </summary>
+/// <param name="templateId">The ID of the template to use for the account.</param>
+/// <returns>
+/// The BurnsUriParameterBuilder object.
+/// </returns>
         public BurnsUriParameterBuilder WithTemplateId(string templateId)
         {
             _templateId = templateId;
             return this;
         }
 
+
+/// <summary>
+/// `WithCollectionBlacklist` is a function that takes an array of strings and returns an
+/// `BurnsUriParameterBuilder` object
+/// </summary>
+/// <param name="collectionBlacklist">A list of collections to exclude from the results.</param>
+/// <returns>
+/// The BurnsUriParameterBuilder object.
+/// </returns>
         public BurnsUriParameterBuilder WithCollectionBlacklist(string[] collectionBlacklist)
         {
             _collectionBlacklist = string.Join(",", collectionBlacklist);
             return this;
         }
 
+
+/// <summary>
+/// `WithCollectionWhitelist` is a function that takes an array of strings and returns an
+/// `BurnsUriParameterBuilder` object
+/// </summary>
+/// <param name="collectionWhitelist">A list of collections to include in the response.</param>
+/// <returns>
+/// The BurnsUriParameterBuilder object.
+/// </returns>
         public BurnsUriParameterBuilder WithCollectionWhitelist(string[] collectionWhitelist)
         {
             _collectionWhitelist = string.Join(",", collectionWhitelist);
             return this;
         }
 
+
+/// <summary>
+/// > This function takes an array of strings and joins them together with a comma
+/// </summary>
+/// <param name="ids">A comma-separated list of account IDs.</param>
+/// <returns>
+/// A string
+/// </returns>
         public BurnsUriParameterBuilder WithIds(string[] ids)
         {
             _ids = string.Join(",", ids);
             return this;
         }
 
+
+/// <summary>
+/// `WithLowerBound` sets the lower bound of the `account_ids` parameter
+/// </summary>
+/// <param name="lowerBound">The lower bound of the accounts to retrieve.</param>
+/// <returns>
+/// The BurnsUriParameterBuilder object.
+/// </returns>
         public BurnsUriParameterBuilder WithLowerBound(string lowerBound)
         {
             _lowerBound = lowerBound;
             return this;
         }
 
+
+/// <summary>
+/// `WithUpperBound` sets the upper bound of the range of accounts to be returned
+/// </summary>
+/// <param name="upperBound">The upper bound of the range to query.</param>
+/// <returns>
+/// The BurnsUriParameterBuilder object.
+/// </returns>
         public BurnsUriParameterBuilder WithUpperBound(string upperBound)
         {
             _upperBound = upperBound;
             return this;
         }
 
+
+/// <summary>
+/// `WithPage` sets the `_page` variable to the value of the `page` parameter
+/// </summary>
+/// <param name="page">The page number of the results to return.</param>
+/// <returns>
+/// The BurnsUriParameterBuilder object.
+/// </returns>
         public BurnsUriParameterBuilder WithPage(int page)
         {
             _page = page;
             return this;
         }
 
+
+/// <summary>
+/// `WithLimit` sets the `_limit` variable to the value of the `limit` parameter
+/// </summary>
+/// <param name="limit">The number of results to return.</param>
+/// <returns>
+/// The BurnsUriParameterBuilder object.
+/// </returns>
         public BurnsUriParameterBuilder WithLimit(int limit)
         {
             _limit = limit;
             return this;
         }
 
+
+/// <summary>
+/// > This function sets the sort strategy for the query
+/// </summary>
+/// <param name="SortStrategy"></param>
+/// <returns>
+/// The builder object itself.
+/// </returns>
         public BurnsUriParameterBuilder WithOrder(SortStrategy sorting)
         {
             _sortStrategy = sorting;
             return this;
         }
 
+
+/// <summary>
+/// It builds a query string based on the parameters that have been set
+/// </summary>
+/// <returns>
+/// A string that contains the parameters for the query.
+/// </returns>
         public string Build()
         {
             var parameterString = new StringBuilder("?");
