fc5697475bc2ac1006f6beabb632dd3520f1f19e
Haron Kipkorir <ronoharonkip@gmail.com>
2022-12-12 16:44:56 +0300

    Moved all the files from Core to the main Project

--- AtomicAssetsApiClient/AtomicAssetsApiFactory.cs
+++ AtomicAssetsApiClient/AtomicAssetsApiFactory.cs
@@ -22,19 +22,26 @@ namespace AtomicAssetsApiClient
         public AccountsApi AccountsApi => new AccountsApi(_baseUrl);
 
         public AssetsApi AssetsApi => new AssetsApi(_baseUrl);
-        
+
         public BurnsApi BurnsApi => new BurnsApi(_baseUrl);
 
         public CollectionsApi CollectionsApi => new CollectionsApi(_baseUrl);
-        
+
         public ConfigApi ConfigApi => new ConfigApi(_baseUrl);
-        
+
         public OffersApi OffersApi => new OffersApi(_baseUrl);
 
         public SchemasApi SchemasApi => new SchemasApi(_baseUrl);
-        
+
         public TemplatesApi TemplatesApi => new TemplatesApi(_baseUrl);
-        
-        public TransfersApi TransfersApi => new TransfersApi(_baseUrl);
+
+        public TransfersApi TransfersApi =>
+
+#if !UNITY
+                new TransfersApi(_baseUrl);
+#else
+            new TranserApi(_baseUrl);
+#endif
+
     }
 }
